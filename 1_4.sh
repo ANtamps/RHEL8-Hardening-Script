e5f889f3301b88da3d80a4c0fd8d2e5c7d5d791a
ANtamps <tampoy.aldrian@gmail.com>
2022-06-08 05:16:29 -0400

    Finished Section 1.4 and added dependencies scripts

--- 1_4.sh
+++ 1_4.sh
@@ -1,7 +1,34 @@
-$password="insert"
+#!/bin/bash
 
-printf '%s\n' $password $password | script -qf -c 'grub2-setpassword' /dev/null
+# Set grub2-password
 
+nohup bash dependencies_script/grub2-setpw.sh >/dev/null 2>&1
+echo "Grub2 password set"
 
-# hello
+# Update the grub2 configuration
+
+grub2-mkconfig -o "$(dirname "$(find /boot -type f \( -name 'grubenv' -o -name 'grub.conf' -o -name 'grub.cfg' \) -exec grep -Pl '^\h*(kernelopts=|linux|kernel)' {} \;)")/grub.cfg"
+
+# Ensure permissions on bootloader config are configured
+
+echo "Setting ownership and permissions for grub.cfg..."
+
+[ -f /boot/grub2/grub.cfg ] && chown root:root /boot/grub2/grub.cfg
+[ -f /boot/grub2/grub.cfg ] && chmod og-rwx /boot/grub2/grub.cfg
+
+echo "Setting ownership and permissions for grubenv..."
+
+[ -f /boot/grub2/grubenv ] && chown root:root /boot/grub2/grubenv
+[ -f /boot/grub2/grubenv ] && chmod og-rwx /boot/grub2/grubenv
+
+echo "Setting ownership and permissions for user.cfg..."
+
+[ -f /boot/grub2/user.cfg ] && chown root:root /boot/grub2/user.cfg
+[ -f /boot/grub2/user.cfg ] && chmod og-rwx /boot/grub2/user.cfg
+
+# Ensure authentication is required when booting into rescue mode
+
+if grep -q /systemd-sulogin-shell /usr/lib/systemd/system/rescue.service; then
+	echo "Login authentication when rescue mode is set, continuing..."
+fi
 
--- 1_4_3.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-echo -e "[Service]\nExecStart=-/usr/lib/systemd/systemd-sulogin-shell rescue" > /etc/systemd/system/rescue.service.d/00-require-auth.conf
