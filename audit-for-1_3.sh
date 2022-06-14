1ba70b0be115fc2fb85f0ea12884acb79fd13593
jjemicamae <j.benlot24@gmail.com>
2022-06-13 22:54:58 -0400

    Audit script for 1.3

--- audit-for-1_3.sh
+++ audit-for-1_3.sh
@@ -1,10 +1,12 @@
-#!/bin/bash
+##!/bin/bash
 
 ##1.3 Filesystem Integrity Checking##
 ##1.3.1 Ensure AIDE is installed##
 
-if ! rpm -q aide &> /dev/null; then
+if  rpm -q aide &> /dev/null; then
+	rpm -q aide
 	echo -e "AIDE is already installed"
+
 else
 	echo -e "AIDE needs to be installed"
 fi
