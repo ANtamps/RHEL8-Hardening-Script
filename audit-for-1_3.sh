fe54ebc78482a9b53026dbf9e2ab5ce3e04fadf2
jjemicamae <j.benlot24@gmail.com>
2022-06-14 02:46:00 -0400

    Audit for 1.3

--- audit-for-1_3.sh
+++ audit-for-1_3.sh
@@ -1,12 +1,23 @@
 ##!/bin/bash
+##AUDIT##
 
 ##1.3 Filesystem Integrity Checking##
 ##1.3.1 Ensure AIDE is installed##
 
 if  rpm -q aide &> /dev/null; then
 	rpm -q aide
-	echo -e "AIDE is already installed"
+	echo -e "AIDE is already installed: \033[1;32mOK\033[0m"
 
 else
-	echo -e "AIDE needs to be installed"
+	echo -e "AIDE needs to be installed: \033[1;32mOK\033[0m"
 fi
+
+
+##1.3.2 Ensure filesystem integrity is regularly checked##
+
+if  grep -Ers '^([^#]+\s+)?(\/usr\/s?bin\/|^\s*)aide(\.wrapper)?\s(--?\S+\s)*(--(check|update)|\$AIDEARGS)\b' /etc/cron.* /etc/crontab /var/spool/cron/ &> /dev/null; then
+	echo -e "Check: \033[1;32mOK\033[0m"
+else
+	echo -e "Check: \033[1;31mERROR\033[0m"
+fi
+
