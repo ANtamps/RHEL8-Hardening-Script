9a6f81b0121830c74335f8e3128b0ea16e9e56ce
ANtamps <86394721+ANtamps@users.noreply.github.com>
2022-06-15 15:43:40 +0800

    Fixed audit for 1.4 to 1.6

--- audit-for-1_4-to-1_6.sh
+++ audit-for-1_4-to-1_6.sh
@@ -1,29 +1,79 @@
 #!/bin/bash
 
-{ tst1="" tst2="" output="" grubdir=$(dirname "$(find /boot -type f \( -name 'grubenv' -o -name 'grub.conf' -o -name 'grub.cfg' \) -exec grep -El '^\s*(kernelopts=|linux|kernel)' {} \;)") if [ -f "$grubdir/user.cfg" ]; then grep -Pq '^\h*GRUB2_PASSWORD\h*=\h*.+$' "$grubdir/user.cfg" && output="bootloader password set in \"$grubdir/user.cfg\"" fi if [ -z "$output" ]; then grep -Piq '^\h*set\h+superusers\h*=\h*"?[^"\n\r]+"?(\h+.*)?$' "$grubdir/grub.cfg" && tst1=pass grep -Piq '^\h*password(_pbkdf2)?\h+\H+\h+.+$' "$grubdir/grub.cfg" && tst2=pass [ "$tst1" = pass ] && [ "$tst2" = pass ] && output="bootloader password set in \"$grubdir/grub.cfg\"" fi [ -n "$output" ] && echo -e "\n\n PASSED! $output\n\n" }
+if grep "set superusers" /boot/grub2/grub.cfg &> /dev/null; then
+    echo "Superuser is set: \033[1;32mOK\033[0m"
+else
+    echo "Superuser is not set! \033[1;31mERROR\033[0m"
+fi
 
-{ output="" output2="" output3="" output4="" grubdir=$(dirname "$(find /boot -type f \( -name 'grubenv' -o -name 'grub.conf' -o -name 'grub.cfg' \) -exec grep -Pl '^\h*(kernelopts=|linux|kernel)' {} \;)") for grubfile in $grubdir/user.cfg $grubdir/grubenv $grubdir/grub.cfg; do if [ -f "$grubfile" ]; then if stat -c "%a" "$grubfile" | grep -Pq '^\h*[0-7]00$'; then output="$output\npermissions on \"$grubfile\" are \"$(stat -c "%a" "$grubfile")\"" else output3="$output3\npermissions on \"$grubfile\" are \"$(stat -c "%a" "$grubfile")\"" fi if stat -c "%u:%g" "$grubfile" | grep -Pq '^\h*0:0$'; then output2="$output2\n\"$grubfile\" is owned by \"$(stat -c "%U" "$grubfile")\" and belongs to group \"$(stat -c "%G" "$grubfile")\"" else output4="$output4\n\"$grubfile\" is owned by \"$(stat -c "%U" "$grubfile")\" and belongs to group \"$(stat -c "%G" "$grubfile")\"" fi fi done if [[ -n "$output" && -n "$output2" && -z "$output3" && -z "$output4" ]]; then echo -e "\nPASSED:" [ -n "$output" ] && echo -e "$output" [ -n "$output2" ] && echo -e "$output2" else echo -e "\nFAILED:" [ -n "$output3" ] && echo -e "$output3" [ -n "$output4" ] && echo -e "$output4" fi }
+if grep "password" /boot/grub2/grub.cfg &> /dev/null; then 
+    echo "Grub2 password is set: \033[1;32mOK\033[0m"
+else
+    echo "Grub2 password not set! \033[1;31mERROR\033[0m"
+fi
 
-grep -r /systemd-sulogin-shell /usr/lib/systemd/system/rescue.service /etc/systemd/system/rescue.service.d
+if grep -r /systemd-sulogin-shell || grep -r /usr/lib/systemd/system/rescue.service || grep -r /etc/systemd/system/rescue.service.d; then
+    echo "Authentication in rescue mode set: "
+else
+    echo "Authentication in rescue mode not set!"
+fi
 
-grep -i '^\s*storage\s*=\s*none' /etc/systemd/coredump.conf
+if grep -i '^\s*storage\s*=\s*none' /etc/systemd/coredump.conf &> /dev/null; then
+    echo "Coredump storage equal to none: "
+else
+    echo "Coredump not set to none!"
+fi
 
-grep -i '^\s*ProcessSizeMax\s*=\s*0' /etc/systemd/coredump.conf
+if grep -i '^\s*ProcessSizeMax\s*=\s*0' /etc/systemd/coredump.conf &> /dev/null; then
+    echo "Coredump ProcessSizeMax equal to 0:"
+else
+    echo "Coredump ProcessSizeMax not equal to 0!"
+fi
 
-{ krp="" pafile="" fafile="" kpname="kernel.randomize_va_space" kpvalue="2" searchloc="/run/sysctl.d/*.conf /etc/sysctl.d/*.conf /usr/local/lib/sysctl.d/*.conf /usr/lib/sysctl.d/*.conf /lib/sysctl.d/*.conf /etc/sysctl.conf" krp="$(sysctl "$kpname" | awk -F= '{print $2}' | xargs)" pafile="$(grep -Psl -- "^\h*$kpname\h*=\h*$kpvalue\b\h*(#.*)?$" $searchloc)" fafile="$(grep -s -- "^\s*$kpname" $searchloc | grep -Pv -- "\h*=\h*$kpvalue\b\h*" | awk -F: '{print $1}')" if [ "$krp" = "$kpvalue" ] && [ -n "$pafile" ] && [ -z "$fafile" ]; then echo -e "\nPASS:\n\"$kpname\" is set to \"$kpvalue\" in the running configuration and in \"$pafile\"" else echo -e "\nFAIL: " [ "$krp" != "$kpvalue" ] && echo -e "\"$kpname\" is set to \"$krp\" in the running configuration\n" [ -n "$fafile" ] && echo -e "\n\"$kpname\" is set incorrectly in \"$fafile\"" [ -z "$pafile" ] && echo -e "\n\"$kpname = $kpvalue\" is not set in a kernel parameter configuration file\n" fi }
+if sysctl kernel.randomize_va_space -eq 2 &> /dev/null; then
+    echo "Kernerl randomize va space set to 2: "
+else
+    echo "Kernel randomize va space not set to 2!"
+fi
 
-rpm -q libselinux
+if rpm -q libselinux &> /dev/null; then
+    echo "Libselinux is installed: "
+else
+    echo "Libselinux not installed!"
+fi
 
-grep -P -- '^\h*(kernelopts=|linux|kernel)' $(find /boot -type f \( -name 'grubenv' -o -name 'grub.conf' -o -name 'grub.cfg' \) -exec grep -Pl -- '^\h*(kernelopts=|linux|kernel)' {} \;) | grep -E -- '(selinux=0|enforcing=0)'
+if grep "^\s*linux" /boot/grub2/grub.cfg &> /dev/null; then
+    echo "SELinux is enabled at boot time:"
+else   
+    echo "SELinux not enabled at boot time!"
+fi
 
-grep -E '^\s*SELINUXTYPE=(targeted|mls)\b' /etc/selinux/config
+if grep SELINUXTYPE=targeted /etc/selinux/config &> /dev/null; then
+    echo "SELINUXTYPE set to targeted: "
+else
+    echo "SELINUXTYPE not set to targeted!"
+fi
 
-getenforce
+if grep SELINUX=enforcing &> /dev/null; then
+    echo "SELINUX set to enforcing:"
+else   
+    echo "SELINUX not set to enforcing!"
+fi
 
-grep -Ei '^\s*SELINUX=(enforcing|permissive)' /etc/selinux/config
+if ps -eZ | grep unconfined_service_t &> /dev/null; then
+    echo "unconfined_service_t not running:"
+else
+    echo "unconfined_service_t is running!"
+fi
 
-ps -eZ | grep unconfined_service_t
+if ! rpm -q setroubleshoot &> /dev/null; then
+    echo "setroubleshoot not installed:"
+else
+    echo "setroubleshoot is installed!"
+fi
 
-rpm -q setroubleshoot
-
-rpm -q mcstrans
+if ! rpm -q mcstrans &> /dev/null; then
+    echo "mcstrans not installed:"
+else
+    echo "mcstrans is installed!"
+fi
