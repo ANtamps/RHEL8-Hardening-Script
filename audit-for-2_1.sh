e319ebcc0f0279ea0c33de09b916e5d661d15dbd
jjemicamae <j.benlot24@gmail.com>
2022-06-15 08:59:07 -0400

    Audit script for 2.1

--- audit-for-2_1.sh
+++ audit-for-2_1.sh
@@ -11,3 +11,16 @@ if  rpm -q chrony &> /dev/null; then
 else
 	echo -e "CHRONY needs to be installed: \033[1;31mERROR\033[0m"
 fi
+
+##2.1.2 Ensure chrony is configured##
+if  grep -E "^(server|pool)" /etc/chrony.conf; then
+	echo -e "Remote server is configured: \033[1;32mOK\033[0m"
+else
+	echo -e "Remote server is not configured: \033[1;31mERROR\033[0m"
+fi
+
+if   grep ^OPTIONS /etc/sysconfig/chronyd; then
+        echo -e "Command configured: \033[1;32mOK\033[0m"
+else
+    	echo -e "OPTIONS="": \033[1;31mERROR\033[0m"
+fi
