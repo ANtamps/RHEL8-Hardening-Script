6567801c6f2faa0545b62ea925a4d71e2f67d513
jjemicamae <j.benlot24@gmail.com>
2022-06-15 08:38:17 -0400

    Script for 2.1

--- 2_1.sh
+++ 2_1.sh
@@ -9,3 +9,31 @@ else
 	dnf install chrony
 fi
 
+##2.1.2 Ensure chrony is configured##
+
+if test -f /etc/chrony.conf; then
+
+	if grep -q "server <remote-server>" /etc/chrony.conf; then
+
+	echo "Remote server is already configured... Continuing..."
+
+	else
+	echo "server <remote-server>" > /etc/chrony.conf
+	fi
+else
+
+	touch /etc/chrony.conf
+	echo  "server <remote-server>" > /etc/chrony.conf
+
+dconf update
+
+fi
+
+if test -f  /etc/sysconfig/chronyd; then
+
+	echo "OPTIONS=\"-u chrony\""
+
+	else
+	echo -e "Configuring.." 
+	sed -i 's/OPTIONS=""/OPTIONS="-u chrony"/g' /etc/sysconfig/chronyd
+fi
