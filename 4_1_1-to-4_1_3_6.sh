825caec00974f91a8daf0b7b87e50e4a7ba6ca46
jjemicamae <j.benlot24@gmail.com>
2022-06-28 23:02:16 +0800

    Added script for Chapter 4

--- 4_1_1-to-4_1_3_6.sh
+++ 4_1_1-to-4_1_3_6.sh
@@ -22,3 +22,17 @@ grubby --update-kernel ALL --args 'audit=1'
 ##4.1.1.4 Ensure audit_backlog_limit is sufficient
 grubby --update-kernel ALL --args 'audit_backlog_limit=8192'
 
+##4.1.2.1 Ensure audit log storage size is configured
+sed -i 's/max_log_file = 8/max_log_file= 20/g' /etc/audit/auditd.conf
+grep -w "^\s*max_log_file\s*=" /etc/audit/auditd.conf
+
+
+##4.1.2.2 Ensure audit logs are not automatically deleted
+sed -i 's/max_log_file_action = ROTATE/max_log_file_action = keep_logs/g' /etc/audit/auditd.conf
+ grep max_log_file_action /etc/audit/auditd.conf
+
+
+##4.1.2.3 Ensure system is disabled when audit logs are full 
+sed -i 's/space_left_action = SYSLOG/space_left_action = email/g' /etc/audit/auditd.conf
+grep space_left_action /etc/audit/auditd.conf
+grep action_mail_acct /etc/audit/auditd.conf
