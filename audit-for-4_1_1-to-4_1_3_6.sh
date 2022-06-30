686423bbb69a17605a1932e8bd8cdbaf5865de90
jjemicamae <j.benlot24@gmail.com>
2022-06-30 12:06:03 +0800

    Audit script for Chapter 4.1-4.1.3.3

--- audit-for-4_1_1-to-4_1_3_6.sh
+++ audit-for-4_1_1-to-4_1_3_6.sh
@@ -58,130 +58,26 @@ else
 fi
 
 ##4.1.3.1 Ensure changes to system administration scope (sudoers) is collected
-awk '/^ *-w/ \
-&&/\/etc\/sudoers/ \
-&&/ +-p *wa/ \
-&&(/ key= *[!-~]* *$/||/ -k *[!-~]* *$/)' /etc/audit/rules.d/*.rules
+grep -c “scope” /etc/audit/rules.d/50-scope.rules
 
-auditctl -l | awk '/^ *-w/ \
-&&/\/etc\/sudoers/ \
-&&/ +-p *wa/ \
-&&(/ key= *[!-~]* *$/||/ -k *[!-~]* *$/)'
-
+auditctl -l | grep -c “scope”
 
 ##4.1.3.2 Ensure actions as another user are always logged
-awk '/^ *-a *always,exit/ \
-&&/ -F *arch=b[2346]{2}/ \
-&&(/ -F *auid!=unset/||/ -F *auid!=-1/||/ -F *auid!=4294967295/) \
-&&(/ -C *euid!=uid/||/ -C *uid!=euid/) \
-&&/ -S *execve/ \
-&&(/ key= *[!-~]* *$/||/ -k *[!-~]* *$/)' /etc/audit/rules.d/*.rules
+grep -c “user_emulation” /etc/audit/rules.d/50-user_emulation.rules
 
- auditctl -l | awk '/^ *-a *always,exit/ \
-&&/ -F *arch=b[2346]{2}/ \
-&&(/ -F *auid!=unset/||/ -F *auid!=-1/||/ -F *auid!=4294967295/) \
-&&(/ -C *euid!=uid/||/ -C *uid!=euid/) \
-&&/ -S *execve/ \
-&&(/ key= *[!-~]* *$/||/ -k *[!-~]* *$/)'
-
+auditctl -l | grep -c “user_emulation”
 
 ##4.1.3.3 Ensure events that modify the sudo log file are collected
+grep -c “sudo_log_file” /etc/audit/rules.d/50-sudo.rules
 
-SUDO_LOG_FILE_ESCAPED=$(grep -r logfile /etc/sudoers* | sed -e 
-
-'s/.*logfile=//;s/,? .*//' -e 's/"//g' -e 's|/|\\/|g')
- [ -n "${SUDO_LOG_FILE_ESCAPED}" ] && awk "/^ *-w/ \
-&&/"${SUDO_LOG_FILE_ESCAPED}"/ \
-&&/ +-p *wa/ \
-&&(/ key= *[!-~]* *$/||/ -k *[!-~]* *$/)" /etc/audit/rules.d/*.rules \ || printf "ERROR: Variable 'SUDO_LOG_FILE_ESCAPED' is unset.\n"
-
-'s/.*logfile=//;s/,? .*//' -e 's/"//g' -e 's|/|\\/|g')
- [ -n "${SUDO_LOG_FILE_ESCAPED}" ] && auditctl -l | awk "/^ *-w/ \
-&&/"${SUDO_LOG_FILE_ESCAPED}"/ \
-&&/ +-p *wa/ \
-&&(/ key= *[!-~]* *$/||/ -k *[!-~]* *$/)" \ || printf "ERROR: Variable 'SUDO_LOG_FILE_ESCAPED' is unset.\n"
+auditctl -l | grep -c “sudo_log_file”
 
 ##4.1.3.4 Ensure events that modify date and time information are collected
+grep -c “time-change” /etc/audit/rules.d/50-time-change.rules
 
-awk '/^ *-a *always,exit/ \
-&&/ -F *arch=b[2346]{2}/ \
-&&/ -S/ \
-&&(/adjtimex/ \
- ||/settimeofday/ \
- ||/clock_settime/ ) \
-&&(/ key= *[!-~]* *$/||/ -k *[!-~]* *$/)' /etc/audit/rules.d/*.rules
-
-awk '/^ *-w/ \
-&&/\/etc\/localtime/ \
-&&/ +-p *wa/ \
-&&(/ key= *[!-~]* *$/||/ -k *[!-~]* *$/)' /etc/audit/rules.d/*.rules
-
- auditctl -l | awk '/^ *-a *always,exit/ \
-&&/ -F *arch=b[2346]{2}/ \
-&&/ -S/ \
-&&(/adjtimex/ \
- ||/settimeofday/ \
- ||/clock_settime/ ) \
-&&(/ key= *[!-~]* *$/||/ -k *[!-~]* *$/)'
-
- auditctl -l | awk '/^ *-w/ \
-&&/\/etc\/localtime/ \
-&&/ +-p *wa/ \
-&&(/ key= *[!-~]* *$/||/ -k *[!-~]* *$/)'
+auditctl -l | grep -c “time-change”
 
 ##4.1.3.5 Ensure events that modify the system's network environment are collected
+grep -c “system-locale” /etc/audit/rules.d/50-system_local.rules
 
- awk '/^ *-a *always,exit/ \
-&&/ -F *arch=b[2346]{2}/ \
-&&/ -S/ \
-&&(/sethostname/ \
- ||/setdomainname/) \
-&&(/ key= *[!-~]* *$/||/ -k *[!-~]* *$/)' /etc/audit/rules.d/*.rules
-
- awk '/^ *-w/ \
-&&(/\/etc\/issue/ \
- ||/\/etc\/issue.net/ \
- ||/\/etc\/hosts/ \
- ||/\/etc\/sysconfig\/network/) \
-&&/ +-p *wa/ \
-&&(/ key= *[!-~]* *$/||/ -k *[!-~]* *$/)' /etc/audit/rules.d/*.rules
-
-auditctl -l | awk '/^ *-a *always,exit/ \
-&&/ -F *arch=b[2346]{2}/ \
-&&/ -S/ \
-&&(/sethostname/ \
- ||/setdomainname/) \
-&&(/ key= *[!-~]* *$/||/ -k *[!-~]* *$/)'
-
- auditctl -l | awk '/^ *-w/ \
-&&(/\/etc\/issue/ \
- ||/\/etc\/issue.net/ \
- ||/\/etc\/hosts/ \
- ||/\/etc\/sysconfig\/network/) \
-&&/ +-p *wa/ \
-&&(/ key= *[!-~]* *$/||/ -k *[!-~]* *$/)'
-
-
-##4.1.3.6 Ensure use of privileged commands are collected
-
-for PARTITION in $(findmnt -n -l -k -it $(awk '/nodev/ { print $2 }' 
-/proc/filesystems | paste -sd,) | grep -Pv "noexec|nosuid" | awk '{print 
-$1}'); do
- for PRIVILEGED in $(find "${PARTITION}" -xdev -perm /6000 -type f); do
- grep -qr "${PRIVILEGED}" /etc/audit/rules.d && printf "OK: 
-'${PRIVILEGED}' found in auditing rules.\n" || printf "Warning: 
-'${PRIVILEGED}' not found in on disk configuration.\n"
- done
-done
-
-RUNNING=$(auditctl -l)
-[ -n "${RUNNING}" ] && for PARTITION in $(findmnt -n -l -k -it $(awk 
-'/nodev/ { print $2 }' /proc/filesystems | paste -sd,) | grep -Pv 
-"noexec|nosuid" | awk '{print $1}'); do
- for PRIVILEGED in $(find "${PARTITION}" -xdev -perm /6000 -type f); do
- printf -- "${RUNNING}" | grep -q "${PRIVILEGED}" && printf "OK: 
-'${PRIVILEGED}' found in auditing rules.\n" || printf "Warning: 
-'${PRIVILEGED}' not found in running configuration.\n"
- done
-done \ || printf "ERROR: Variable 'RUNNING' is unset.\n"
-
+auditctl -l | grep -c “system-locale”
