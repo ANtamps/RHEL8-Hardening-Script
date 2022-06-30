1e5ea78735f21cc1076db0efce7d8a307c3e6122
jjemicamae <j.benlot24@gmail.com>
2022-06-30 11:26:48 +0800

    Audit script for Chapter 4.1-4.1.3.3

--- audit-for-4_1_1-to-4_1_3_6.sh
+++ audit-for-4_1_1-to-4_1_3_6.sh
@@ -83,3 +83,103 @@ else
   echo -e "Rules not loaded: \033[1;31mERROR\033[0m"
 fi
 
+##4.1.3.3 Ensure events that modify the sudo log file are collected
+
+SUDO_LOG_FILE_ESCAPED=$(grep -r logfile /etc/sudoers* | sed -e 
+
+'s/.*logfile=//;s/,? .*//' -e 's/"//g' -e 's|/|\\/|g')
+# [ -n "${SUDO_LOG_FILE_ESCAPED}" ] && awk "/^ *-w/ \
+&&/"${SUDO_LOG_FILE_ESCAPED}"/ \
+&&/ +-p *wa/ \
+&&(/ key= *[!-~]* *$/||/ -k *[!-~]* *$/)" /etc/audit/rules.d/*.rules \ || printf "ERROR: Variable 'SUDO_LOG_FILE_ESCAPED' is unset.\n"
+
+'s/.*logfile=//;s/,? .*//' -e 's/"//g' -e 's|/|\\/|g')
+# [ -n "${SUDO_LOG_FILE_ESCAPED}" ] && auditctl -l | awk "/^ *-w/ \
+&&/"${SUDO_LOG_FILE_ESCAPED}"/ \
+&&/ +-p *wa/ \
+&&(/ key= *[!-~]* *$/||/ -k *[!-~]* *$/)" \ || printf "ERROR: Variable 'SUDO_LOG_FILE_ESCAPED' is unset.\n"
+
+##4.1.3.4 Ensure events that modify date and time information are collected
+
+awk '/^ *-a *always,exit/ \
+&&/ -F *arch=b[2346]{2}/ \
+&&/ -S/ \
+&&(/adjtimex/ \
+ ||/settimeofday/ \
+ ||/clock_settime/ ) \
+&&(/ key= *[!-~]* *$/||/ -k *[!-~]* *$/)' /etc/audit/rules.d/*.rules
+
+awk '/^ *-w/ \
+&&/\/etc\/localtime/ \
+&&/ +-p *wa/ \
+&&(/ key= *[!-~]* *$/||/ -k *[!-~]* *$/)' /etc/audit/rules.d/*.rules
+
+ auditctl -l | awk '/^ *-a *always,exit/ \
+&&/ -F *arch=b[2346]{2}/ \
+&&/ -S/ \
+&&(/adjtimex/ \
+ ||/settimeofday/ \
+ ||/clock_settime/ ) \
+&&(/ key= *[!-~]* *$/||/ -k *[!-~]* *$/)'
+
+ auditctl -l | awk '/^ *-w/ \
+&&/\/etc\/localtime/ \
+&&/ +-p *wa/ \
+&&(/ key= *[!-~]* *$/||/ -k *[!-~]* *$/)'
+
+##4.1.3.5 Ensure events that modify the system's network environment are collected
+
+ awk '/^ *-a *always,exit/ \
+&&/ -F *arch=b[2346]{2}/ \
+&&/ -S/ \
+&&(/sethostname/ \
+ ||/setdomainname/) \
+&&(/ key= *[!-~]* *$/||/ -k *[!-~]* *$/)' /etc/audit/rules.d/*.rules
+
+ awk '/^ *-w/ \
+&&(/\/etc\/issue/ \
+ ||/\/etc\/issue.net/ \
+ ||/\/etc\/hosts/ \
+ ||/\/etc\/sysconfig\/network/) \
+&&/ +-p *wa/ \
+&&(/ key= *[!-~]* *$/||/ -k *[!-~]* *$/)' /etc/audit/rules.d/*.rules
+
+auditctl -l | awk '/^ *-a *always,exit/ \
+&&/ -F *arch=b[2346]{2}/ \
+&&/ -S/ \
+&&(/sethostname/ \
+ ||/setdomainname/) \
+&&(/ key= *[!-~]* *$/||/ -k *[!-~]* *$/)'
+
+ auditctl -l | awk '/^ *-w/ \
+&&(/\/etc\/issue/ \
+ ||/\/etc\/issue.net/ \
+ ||/\/etc\/hosts/ \
+ ||/\/etc\/sysconfig\/network/) \
+&&/ +-p *wa/ \
+&&(/ key= *[!-~]* *$/||/ -k *[!-~]* *$/)'
+
+
+##4.1.3.6 Ensure use of privileged commands are collected
+
+for PARTITION in $(findmnt -n -l -k -it $(awk '/nodev/ { print $2 }' 
+/proc/filesystems | paste -sd,) | grep -Pv "noexec|nosuid" | awk '{print 
+$1}'); do
+ for PRIVILEGED in $(find "${PARTITION}" -xdev -perm /6000 -type f); do
+ grep -qr "${PRIVILEGED}" /etc/audit/rules.d && printf "OK: 
+'${PRIVILEGED}' found in auditing rules.\n" || printf "Warning: 
+'${PRIVILEGED}' not found in on disk configuration.\n"
+ done
+done
+
+RUNNING=$(auditctl -l)
+[ -n "${RUNNING}" ] && for PARTITION in $(findmnt -n -l -k -it $(awk 
+'/nodev/ { print $2 }' /proc/filesystems | paste -sd,) | grep -Pv 
+"noexec|nosuid" | awk '{print $1}'); do
+ for PRIVILEGED in $(find "${PARTITION}" -xdev -perm /6000 -type f); do
+ printf -- "${RUNNING}" | grep -q "${PRIVILEGED}" && printf "OK: 
+'${PRIVILEGED}' found in auditing rules.\n" || printf "Warning: 
+'${PRIVILEGED}' not found in running configuration.\n"
+ done
+done \ || printf "ERROR: Variable 'RUNNING' is unset.\n"
+
